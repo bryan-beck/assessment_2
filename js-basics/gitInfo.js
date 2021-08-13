3e55287f0eca03c4f1973f7a3d45ee3c8bcc3e6a
Bryan Beck <88339907+bryan-beck@users.noreply.github.com>
2021-08-13 09:33:42 -0700

    Add files via upload
    assessment_2 Complete

---
+++ js-basics/gitInfo.js
@@ -0,0 +1,96 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+let gitDefinition = 'Software for tracking changes in any set of files.'
+//CODE HERE
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+let gitHubDefinition = 'A repository that enables collaboration on projects.'
+//CODE HERE
+
+//////////////////PROBLEMS 3 - 8////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+//CODE HERE
+let init = {
+    description: "The init function initialises what you call with it",
+    code: "function init()",
+     }
+
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+//CODE HERE
+let clone = {
+    description: 'clone is a method in the Java programming language for object duplication'
+    code: 'clone() '
+}
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+//CODE HERE
+let status = {
+    description: 'The status property sets the text in the status bar at the bottom of the browser, or returns the previously set text.'
+    code: 'window.status'
+}
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+//CODE HERE
+let add = {
+    description: 'The add() method appends a new element with a specified value to the end of a Set object.'
+    code: 'set1.add(42);'
+}
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+//CODE HERE
+let commit = {
+    description: 'A commit command is used to fetch updates from the staging area to the repository.'
+    code: '$ git commit'
+}
+
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+let push = {
+    description: 'push() is a method that adds one or more elements to the end of an array.'
+    code: 'color.push("purple");'
+}
+//CODE HERE
---
+++ js-basics/variables.js
@@ -0,0 +1,38 @@
+//////////////////PROBLEM 1////////////////////
+/* Create a variable called 'myName' with a value that is your name as a string. */
+let myName = 'Bryan'
+//CODE HERE 
+
+//////////////////PROBLEM 2////////////////////
+/* Create a variable called 'faveNum' with a value that is your favorite number. */
+let faveNum = 10
+//CODE HERE
+
+//////////////////PROBLEM 3////////////////////
+/* Create a variable called 'lovesCode' and assign it a boolean value. */
+let lovesCode = True
+//CODE HERE
+
+//////////////////PROBLEM 4////////////////////
+/*
+  Write an if-statement that checks if the faveNum variable is 
+  equal to 13. If it is, console.log('lucky 13!'). 
+  If it's not, console.log('not lucky 13').
+*/
+if (faveNum = 13)
+{(console.log 'lucky 13')
+}
+{else (console.log 'not lucky 13')
+}
+//CODE HERE
+
+//////////////////PROBLEM 5////////////////////
+/*
+  Write a for loop that prints out your faveNum 5 times.
+*/
+for (let i = 0; i <5; i++) {
+  [1,2,3,4,5].forEach(function(i) {
+    faveNum(console.log (i));
+  })
+  }
+//CODE HERE
