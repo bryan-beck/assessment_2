47f44ec2d74b2e77c98a2b92ca844ec274c774d1
Bryan Beck <88339907+bryan-beck@users.noreply.github.com>
2021-08-26 15:17:15 -0700

    Update password.js
    adding changes

--- js-basics/password.js
+++ js-basics/password.js
@@ -1,16 +1,39 @@
-function greet(name) {
-    console.log("Hello, " + name);
-  }
+// function greet(name) {
+//     console.log("Hello, " + name);
+//   }
   
-  greet("User");
+//   greet("User");
 
-  var myInput = document.getElementById("password");
-  var length = document.getElementById("length");
-  if(myInput.value.length >= 10) {
-    length.classList.remove("failure");
-    length.classList.add("success");
-  } else {
-    length.classList.remove("success");
-    length.classList.add("failure");
-  }
-}
+//   var myInput = document.getElementById("password");
+//   var length = document.getElementById("length");
+//   if(myInput.value.length >= 10) {
+//     length.classList.remove("failure");
+//     length.classList.add("success");
+//   } else {
+//     length.classList.remove("success");
+//     length.classList.add("failure");
+//   }
+// }
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+console.log('Welcome to the password validator!')
+reader.question("Please enter a password of at least 10 characters ", function(password){
+  if (password.length < 10) {
+     console.log('password not long enough')
+     } else {
+     console.log("Password accepted")
+     }
+       
+ reader.close()
+});
+    
+        
+            
+           
+
